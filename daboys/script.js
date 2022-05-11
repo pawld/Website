bf05f02be0710d6466ab20e8df3810c78797a075
pawld <99927420+pawld@users.noreply.github.com>
2022-05-11 15:36:12 -0700

    Update script.js

--- daboys/script.js
+++ daboys/script.js
@@ -4,6 +4,6 @@ setInterval(()=>{
     counter.innerText = count;
     count++
     
-    if(count > 3) location.replace('https://discord.gg/b2q4rfk823')
+    if(count > 3) location.replace('https://discord.gg/9CPANrfVvH')
     
 },1000)
